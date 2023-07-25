a9d7f7c91282431581bf988d19ebcae5ab2a0e26
ssong <59350987+joojoo1226@users.noreply.github.com>
2023-07-25 22:04:26 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT 
+    id, name, host_id
+FROM 
+    places
+WHERE 
+    host_id IN (
+        SELECT host_id
+        FROM places
+        GROUP BY host_id
+        HAVING COUNT(*) > 1
+    )
+ORDER BY id;
